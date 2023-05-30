8c06b2a3903eab712154de091ee49d732d5be198
PintosAlfredo <pintosalfredo1@gmail.com>
2023-05-30 12:50:59 -0300

    Deploy

--- app.js
+++ app.js
@@ -6,20 +6,10 @@ const app = express();
 const publicPath = path.resolve(__dirname, './public'); 
 app.use(express.static(publicPath) );
 
-
-const port = process.env.PORT || 3000;
-app.listen(port,() => console.log('Servidor corriendo en el puerto ${port}'));
-
-
+app.listen(3000, ()=> {
+        console.log('Servidor corriendo en el puerto 3000')
+});
 
 app.get('/', (req, res) => {
-        res.sendFile(path.join(__dirname, './views/home.html'))
-});
-
-app.get('/register', (req, res) => {
-        res.sendFile(path.join(__dirname, './views/register.html'))
-});
-
-app.get('/login', (req, res) => {
-        res.sendFile(path.join(__dirname, './views/login.html'))
+        res.sendFile(path.join(__dirname, '/views/home.html'))
 });
